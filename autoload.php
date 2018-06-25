ee6e559bf888117378e3a57e77b6d427d2a41bfb
wuyake <428878917@qq.com>
2018-06-25 11:24:42 +0800

    调整位置

---
+++ autoload.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by arkewu
+ * User: arkewu
+ * Date: 2018-06-25 10:11:23
+ * Time: 10:11
+ */
+require_once __DIR__ . '/sdk/src/PlatformApiInterface.php';
+require_once __DIR__ . '/sdk/src/XML2Array.php';
+require_once __DIR__ . '/sdk/src/XmlGenerator.php';
+require_once __DIR__ . '/sdk/src/EbaySession.php';
+require_once __DIR__ . '/sdk/src/EbayApiAbstract.php';
